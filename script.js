1657f03cfaf94246b3e677baee632444a0a86ec8
Booligoosh <booligoosh@optusnet.com.au>
2016-06-28 14:15:33 +1000

    removed stopSolve() from arrow keys

--- script.js
+++ script.js
@@ -177,28 +177,24 @@ function keyDown() {
   if (event.keyCode === 37) {
     if (solveStarted === true) {
       solves1.push(solve);
-      stopSolve();
     }
   }
   //up
   if (event.keyCode === 38) {
     if (solveStarted === true) {
       solves2.push(solve);
-      stopSolve();
     }
   }
   //right
   if (event.keyCode === 39) {
     if (solveStarted === true) {
       solves3.push(solve);
-      stopSolve();
     }
   }
   //down
   if (event.keyCode === 40) {
     if (solveStarted === true) {
       solves4.push(solve);
-      stopSolve();
     }
   }
 }
