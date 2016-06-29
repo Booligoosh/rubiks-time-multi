08158098ca82b9d6350d4c60fee1bc9e79a5c033
Booligoosh <booligoosh@optusnet.com.au>
2016-06-29 12:25:40 +1000

    made arrows go white not green once done

--- script.js
+++ script.js
@@ -190,7 +190,7 @@ function keyDown() {
   if (event.keyCode === 37) {
     if (solveStarted === true && player1done === false) {
       player1done = true;
-      document.getElementById("1").style = "-webkit-filter: hue-rotate(90deg) brightness(200%); filter: hue-rotate(90deg) brightness(200%)";
+      document.getElementById("1").style = "-webkit-filter: brightness(200%); filter: brightness(200%)";
       solves1.push(solve);
       playersDone = playersDone + 1;
       if (playersDone >= 4) {
@@ -202,7 +202,7 @@ function keyDown() {
   if (event.keyCode === 38) {
     if (solveStarted === true && player2done === false) {
       player2done = true;
-      document.getElementById("2").style = "-webkit-filter: hue-rotate(90deg) brightness(200%); filter: hue-rotate(90deg) brightness(200%)";
+      document.getElementById("2").style = "-webkit-filter: brightness(200%); filter: brightness(200%)";
       solves2.push(solve);
       playersDone = playersDone + 1;
       if (playersDone >= 4) {
@@ -214,7 +214,7 @@ function keyDown() {
   if (event.keyCode === 39) {
     if (solveStarted === true && player3done === false) {
       player3done = true;
-      document.getElementById("3").style = "-webkit-filter: hue-rotate(90deg) brightness(200%); filter: hue-rotate(90deg) brightness(200%)";
+      document.getElementById("3").style = "-webkit-filter: brightness(200%); filter: brightness(200%)";
       solves3.push(solve);
       playersDone = playersDone + 1;
       if (playersDone >= 4) {
@@ -226,7 +226,7 @@ function keyDown() {
   if (event.keyCode === 40) {
     if (solveStarted === true && player4done === false) {
       player4done = true;
-      document.getElementById("4").style = "-webkit-filter: hue-rotate(90deg) brightness(200%); filter: hue-rotate(90deg) brightness(200%)";
+      document.getElementById("4").style = "-webkit-filter: brightness(200%); filter: brightness(200%)";
       solves4.push(solve);
       playersDone = playersDone + 1;
       if (playersDone >= 4) {
